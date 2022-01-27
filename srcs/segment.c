e29625ba8ebd5a9694337ba738315bfb6b7d6be0
vsaltel <saltel.valentin@gmail.com>
2022-01-27 15:35:35 +0100

    PT_NOTE

--- srcs/segment.c
+++ srcs/segment.c
@@ -66,62 +66,61 @@ int			init_segments(t_segments *seg, Elf64_Ehdr *elf_hdr, char *binary)
 	return (0);
 }
 
+int			get_lastest_segment(Elf64_Ehdr *elf_hdr, Elf64_Phdr *tmp)
+{
+	size_t		i;
+	size_t		y;
+	size_t		len_tmp;
+
+	i = -1;
+	y = 0;
+	len_tmp = 0;
+	while (++i < elf_hdr->e_phnum)
+		if (tmp[i].p_vaddr > len_tmp)
+		{
+			y = i;
+			len_tmp = tmp[i].p_vaddr;
+		}
+	return (y);
+}
+
 void		edit_segment_size_loop(t_woody *woody, t_segments *lseg, size_t len)
 {
-	Elf64_Phdr	*segment;
-	//Elf64_Phdr	*tmp;
-	//size_t		i;
-	(void)woody;
+	Elf64_Phdr	*tmp;
+	size_t		y;
 
-	segment = lseg->hdata;
+	ft_printf("p_memsz %ld, p_filesz %ld, p_align %ld\n", lseg->hdata->p_memsz, lseg->hdata->p_filesz, lseg->hdata->p_align);
+	ft_printf("p_vaddr %ld, p_offset %ld\n", lseg->hdata->p_vaddr, lseg->hdata->p_offset);
+	ft_printf("p_flags %c%c%c\n", (lseg->hdata->p_flags & PF_R ? 'R' : ' '), (lseg->hdata->p_flags & PF_W ? 'W' : ' '), (lseg->hdata->p_flags & PF_X ? 'X' : ' '));
 
-	ft_printf("p_memsz %ld, p_filesz %ld, p_align %ld\n", segment->p_memsz, segment->p_filesz, segment->p_align);
-	ft_printf("p_vaddr %ld, p_offset %ld\n", segment->p_vaddr, segment->p_offset);
-	ft_printf("p_flags %c%c%c\n", (segment->p_flags & PF_R ? 'R' : ' '), (segment->p_flags & PF_W ? 'W' : ' '), (segment->p_flags & PF_X ? 'X' : ' '));
-
-	segment->p_memsz += len; 
-	segment->p_filesz += len; 
-
-	/*
 	tmp = lseg->begin;
-	i = lseg->hdata_index;
-	while (++i < woody->elf_hdr->e_phnum)
-		if (tmp[i].p_vaddr > segment->p_vaddr)
-		{
-			if ((unsigned char)woody->elf_hdr->e_ident[EI_OSABI] != ELFOSABI_FREEBSD)
-				tmp[i].p_vaddr += len;
-			else
-				tmp[i].p_vaddr = 0;
-			tmp[i].p_offset += len;
-		}
-	*/
+	y = get_lastest_segment(woody->elf_hdr, tmp);
+	ft_printf("lastest : %ld, %ld, %ld\n", woody->elf_hdr->e_shoff, tmp[y].p_offset + tmp[y].p_filesz, tmp[y].p_offset);
+	for (size_t i = tmp[y].p_offset + tmp[y].p_filesz; i < woody->elf_hdr->e_shoff; i++) {
+        ft_printf(" %2x", woody->bindest[i]);
+    }	
+
+	lseg->hdata->p_align = 1;
+	lseg->hdata->p_vaddr = tmp[y].p_vaddr + ((tmp[y].p_memsz / tmp[y].p_align + ((tmp[y].p_memsz % tmp[y].p_align != 0) ? 1 : 0)) * tmp[y].p_align);
+	lseg->hdata->p_offset = tmp[y].p_offset + ((tmp[y].p_filesz / tmp[y].p_align + ((tmp[y].p_filesz % tmp[y].p_align != 0) ? 1 : 0)) * tmp[y].p_align);
+	lseg->hdata->p_memsz = len; 
+	lseg->hdata->p_filesz = len; 
 }
 
 int			add_to_end_segment(t_woody *woody, t_segments *lseg, char *content, size_t content_len)
 {
 	char	*tmp;
-	//int		i;
 
-	/*
 	tmp = malloc(woody->len + content_len);
-	ft_memcpy(tmp, woody->bindest, lseg->hdata->p_offset + lseg->tmp_filesz);
-	i = lseg->hdata->p_offset + lseg->tmp_filesz;
-	ft_memcpy(tmp + i, content, content_len);
-	ft_printf("bntrypoint %d\n", woody->elf_hdr->e_entry);
-	ft_printf("entrypoint %d\n", lseg->hdata->p_vaddr + lseg->tmp_filesz);
-	i += content_len;
-	ft_memcpy(tmp + i, woody->bindest + lseg->hdata->p_offset + lseg->tmp_filesz, woody->len - (lseg->hdata->p_offset + lseg->tmp_filesz));
-	*/
-
-
-	tmp = malloc(woody->len);
-	ft_memcpy(tmp, woody->bindest, woody->len);
-	ft_memcpy(tmp + lseg->hdata->p_offset + lseg->tmp_filesz, content, content_len);
+	ft_memcpy(tmp, woody->bindest, lseg->hdata->p_offset);
+	ft_memcpy(tmp + lseg->hdata->p_offset, content, content_len);
+	//ft_bzero(tmp + lseg->hdata->p_offset + content_len, (((content_len / 4096) + 1) * 4096) - content_len);
+	ft_memcpy(tmp + lseg->hdata->p_offset + content_len, woody->bindest + lseg->hdata->p_offset, woody->len - lseg->hdata->p_offset);
 
 	free(woody->bindest);
 	woody->bindest = tmp;
 	woody->elf_hdr = (Elf64_Ehdr *)woody->bindest;
 	woody->len += content_len;
-	woody->elf_hdr->e_entry = lseg->hdata->p_vaddr + lseg->tmp_filesz;	//entry point
+	woody->elf_hdr->e_entry = lseg->hdata->p_offset;	//entry point
 	return (0);
 }
